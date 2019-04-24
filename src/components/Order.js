d6d9555069a741ff9b1eff0e378e54c107122fb1
Joshua Loran <joshua.loran@gmail.com>
2019-04-24 11:54:11 -0700

    Done.

--- src/components/Order.js
+++ src/components/Order.js
@@ -7,7 +7,11 @@ class Order extends Component {
   }
 
   handleClick = () => {
-    this.state.isClicked = !this.state.isClicked
+    this.setState( prevState => {
+      return {
+        isClicked: !prevState.isClicked
+      }
+    })
   }
 
   render() {
