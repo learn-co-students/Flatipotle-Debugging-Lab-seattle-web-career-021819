01912543a605ffe9ac328c0067ef2175a9bcd6b0
github username <github email address>
2019-04-29 15:40:13 -0700

    done

--- src/components/Form.js
+++ src/components/Form.js
@@ -16,7 +16,7 @@ class Form extends Component {
     ...DEFAULT_STATE
   }
 
-  handleSubmit() {
+  handleSubmit(event) {
     event.preventDefault()
     document.getElementById("order-form").reset()
     this.props.addOrder(this.state)
@@ -26,7 +26,7 @@ class Form extends Component {
     })
   }
 
-  handleChange() {
+  handleChange(event) {
     const itemType = event.target.name
     const item = event.target.value
 
@@ -46,7 +46,7 @@ class Form extends Component {
     return(
       <div className="ui raised container segment">
         <h1 className="ui block header">Order Form</h1>
-        <form className="ui form" id="order-form" onSubmit={ this.handleSubmit }>
+        <form className="ui form" id="order-form" onSubmit={(event) => this.handleSubmit(event) }>
           <ProteinForm
             protein={ this.state.protein }
             handleOnChange={ this.handleChange }
